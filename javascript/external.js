9e8d30867fc341401dac095e3607558780fd2205
AJByers121602 <AJByers121602@gmail.com>
2025-09-18 21:37:37 -0500

    Saved Username and Replaced header with user prompt information.

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,6 @@
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-document.writeln(arr[i],"<br>");
-}
+// arr = ["Mael","Eric","Randall","James"];
+// for (var i=0; i < arr.length; i++)
+// {
+// document.writeln(arr[i],"<br>");
+// }
+user_name =window.prompt("please enter your name","Type your name here");
